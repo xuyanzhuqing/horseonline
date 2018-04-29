e6a2f83e25a6eea1ecdb8a57e272c636e10654b6
yiyong <yiyong@roadoor.com>
2018-04-29 17:21:46 +0800

    1

--- src/modules/template/template.vue
+++ src/modules/template/template.vue
@@ -12,7 +12,7 @@
 		<mt-button type="default">default</mt-button>
 		<mt-button type="primary">primary</mt-button>
 		<mt-button type="danger">danger</mt-button>
-		<p>sdfsdfsdsdsfsdf </p>
+		<p>sdfsdfsdsdsfsdf 按时发斯蒂芬斯蒂芬水电费</p>
 		<!-- <router-view></router-view> -->
 	</div>
 </template>
